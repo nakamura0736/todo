277b2b0607ee6c227af46d19621f1f9f1d3157ea
中村圭輝 <tel07360102@gmail.com>
2020-07-23 15:05:50 +0900

    bootstrap grid幅変更

--- todo/resources/views/tasks.blade.php
+++ todo/resources/views/tasks.blade.php
@@ -2,27 +2,32 @@
 @section('content')
 <div class="container">
 <h1>Task List</h1>
-<form action="/tasks" method="POST">
-  {{ csrf_field() }}
-  <div>
-    <label>Task</label>
+<div class="mb-4">
+  <form action="/tasks" method="POST">
+    {{ csrf_field() }}
     <div>
-      <input type="text" class="form-control" name="name" id="task-name">
+      <label>Task</label>
+      <div class="mb-2">
+        <input type="text" class="form-control" name="name" id="task-name">
+      </div>
     </div>
-  </div>
-  <button type="submit" class="btn btn-primary">Add Task</button>
-</form>
+    <button type="submit" class="btn btn-primary">Add Task</button>
+  </form>
+</div>
 
 <h2>Current Tasks</h2>
 <table class="table">
   <thead>
-    <th>Task</th><th>&nbsp;</th>
+    <tr>
+      <th class="col-8">Task</th>
+      <th class="col-4">&nbsp;</th>
+    <tr>
   </thead>
   <tbody>
     @foreach($tasks as $task)
       <tr>
-        <td>{{ $task->name }}</td>
-        <td>
+        <td >{{ $task->name }}</td>
+        <td >
           <form action="/tasks/{{$task->id}}" method="POST">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
